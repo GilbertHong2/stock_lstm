6f0a2f8ff2c067bf1e31e02e9b4ee3b0b3c67cef
Wanchen <wghong@bu.edu>
2023-08-30 01:43:36 -0700

    added entire model

--- model.py
+++ model.py
@@ -150,5 +150,34 @@ plt.plot(history)
 
 X_test[0]
 
+rnn.eval()
+
+test_predict, _ = rnn(X_test_cuda, hidden_state)
+test_predict_cpu = test_predict.cpu().detach().numpy()
+
+test_predict_cpu.shape
+
+plt.plot(sc.inverse_transform(y_test.reshape(-1,1)))
+plt.plot(sc.inverse_transform(test_predict_cpu.reshape(-1,1)))
+
+# plot original data
+plt.plot(sc.inverse_transform(y.reshape(-1,1)), color='k')
+
+train_pred, hidden_state = rnn(inputs_cuda, None)
+train_pred_cpu = train_pred.cpu().detach().numpy()
+
+# use hidden state from previous training data
+test_predict, _ = rnn(X_test_cuda, hidden_state)
+test_predict_cpu = test_predict.cpu().detach().numpy()
+
+split_pt = int(X.shape[0] * 0.80) + 7 # window_size
+plt.plot(np.arange(7, split_pt, 1), sc.inverse_transform(train_pred_cpu.reshape(-1,1)), color='b')
+plt.plot(np.arange(split_pt, split_pt + len(test_predict_cpu), 1), sc.inverse_transform(test_predict_cpu.reshape(-1,1)), color='r')
+
+# pretty up graph
+plt.xlabel('day')
+plt.ylabel('price of AAL stock')
+plt.legend(['original series','training fit','testing fit'], loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
 
 
