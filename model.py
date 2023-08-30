7221c2e7c6d5c35c99d35372d2911099d4fcff7a
Wanchen <wghong@bu.edu>
2023-08-30 01:41:40 -0700

    added training

--- model.py
+++ model.py
@@ -44,3 +44,111 @@ def processData(data, lb):
     return np.array(X), np.array(Y)
     
 X, y = processData(dataset_cl, 5)
+
+# 3. Split Training and Evaluation datasets
+
+# Since the dataset is a time series data, the data is split temporally. 
+# The earliest 80% of the dataset are used as the training dataset 
+# and the last 20% are used as evaluation
+
+X_train, X_test = X[:int(X.shape[0]*0.80)],X[int(X.shape[0]*0.80):]
+y_train, y_test = y[:int(y.shape[0]*0.80)],y[int(y.shape[0]*0.80):]
+
+# reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], 1, X_train.shape[1]))
+X_test = np.reshape(X_test, (X_test.shape[0], 1, X_test.shape[1]))
+
+# 4. Build and run a RNN regression model
+""" 
+The RNN is built with 2 hidden layers using Keras:
+
+- layer 1 uses an LSTM module with 5 hidden units (the input_shape = (L,1))
+- layer 2 uses a fully connected module with 1 hidden unit
+- the loss used is MSE
+"""
+
+class RNN(nn.Module):
+    def __init__(self, i_size, h_size, n_layers, o_size, dropout=0.1, bidirectional=True):
+        super(RNN, self).__init__()
+        self.num_directions = bidirectional+1
+        self.rnn = nn.LSTM(
+            input_size=i_size,
+            hidden_size=h_size,
+            num_layers=n_layers,
+            dropout=dropout,
+            bidirectional=bidirectional
+        )
+        self.out = nn.Linear(h_size, o_size)
+
+    def forward(self, x, h_state):
+        r_out, hidden_state = self.rnn(x, h_state)
+
+        hidden_size = hidden_state[-1].size(-1)
+        r_out = r_out.view(-1, self.num_directions, hidden_size)
+        outs = self.out(r_out)
+
+        return outs, hidden_state
+    
+    # Global Variables
+
+INPUT_SIZE = 5
+HIDDEN_SIZE = 64
+NUM_LAYERS = 3
+OUTPUT_SIZE = 1
+BATCH_SIZE = 128
+
+# Hyper parameters
+
+learning_rate = 0.001
+num_epochs = 300
+
+rnn = RNN(INPUT_SIZE, HIDDEN_SIZE, NUM_LAYERS, OUTPUT_SIZE,bidirectional=False)
+rnn.cuda()
+
+optimiser = torch.optim.Adam(rnn.parameters(), lr=learning_rate)
+criterion = nn.MSELoss()
+
+hidden_state = None
+
+# rnn
+
+history = []
+X_test_cuda = torch.tensor(X_test).float().cuda()
+y_test_cuda = torch.tensor(y_test).float().cuda()
+
+# we use all the data in one batch
+inputs_cuda = torch.tensor(X_train).float().cuda()
+labels_cuda = torch.tensor(y_train).float().cuda()
+
+for epoch in range(num_epochs):
+    rnn.train()
+    output, _ = rnn(inputs_cuda, hidden_state)
+    # print(output.size())
+
+    loss = criterion(output[:,0,:].view(-1), labels_cuda)
+    optimiser.zero_grad()
+    loss.backward()                     # back propagation
+    optimiser.step()                    # update the parameters
+
+    if epoch % 20 == 0:
+        rnn.eval()
+        test_output, _ = rnn(X_test_cuda, hidden_state)
+        test_loss = criterion(test_output.view(-1), y_test_cuda)
+        print('epoch {}, loss {}, eval loss {}'.format(epoch, loss.item(), test_loss.item()))
+    else:
+        print('epoch {}, loss {}'.format(epoch, loss.item()))
+    history.append(loss.item())
+
+# rnn.out.weight
+
+# for param in rnn.parameters():
+#    print(param.data)
+
+# 5. Model Evaluation
+
+plt.plot(history)
+
+X_test[0]
+
+
+
